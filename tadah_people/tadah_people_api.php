901c577de47c4074e4f1ff3bec8f9cfd524a0c0c
Wang Sha <wangshasg@gmail.com>
2011-03-18 11:06:23 -0400

    convert tap to double space

--- tadah_people/tadah_people_api.php
+++ tadah_people/tadah_people_api.php
@@ -1,17 +1,17 @@
 <?php 
 
 function tadah_people_get_profile($uid = NULL) {
-	if(empty($uid)) {
-		global $user;
-		$uid = $user->$uid;
-	}
-	
-	$profile = db_select('tadah_people', 'p')
-			->fields('p')
-			->condition('deleted', 0)
-			->condition('uid', $uid)
-			->execute()
-			->fetchAssoc();
-	return $profile;
+  if(empty($uid)) {
+    global $user;
+    $uid = $user->$uid;
+  }
+  
+  $profile = db_select('tadah_people', 'p')
+    ->fields('p')
+    ->condition('deleted', 0)
+    ->condition('uid', $uid)
+    ->execute()
+    ->fetchAssoc();
+  return $profile;
 }
 ?>
